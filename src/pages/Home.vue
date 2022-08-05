d7b78936f23fa4d77a2a7c755ff1ea72d38284d3
luc.hoang <luc.hoang@savvycomsoftware.com>
2022-08-05 15:57:35 +0700

    setup

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,20 @@
 <template>
   HelloWorld {{ data }} {{ count }}
   <a-button type="primary">Primary button</a-button>
+
+  <Table />
 </template>
 
 <script>
 import { mapMutations, mapState } from "vuex";
+import Table from "@/components/Table.vue";
 import { API } from "@/api";
 
 export default {
+  components: {
+    Table,
+  },
+
   data() {
     return {
       data: 123,
